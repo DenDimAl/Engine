fbb11ee3502afc807f112db3172f42689740d17f
CrazikMoon <mrkicker525@gmail.com>
2024-02-10 18:27:14 +0400

    fix: перешли на другой код для main

--- SFML_Test1/main.cpp
+++ SFML_Test1/main.cpp
@@ -1,46 +1,37 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include "main.h"
-void checkBorder(sf::CircleShape shape) {
-    if (shape.getPosition().x > 280.00000) {
-        //shape.setPosition(280,shape.getPosition().y);
-        shape.move(-20, 0);
-    }
-    else if (shape.getPosition().x < 0) {
-        shape.setPosition(0, shape.getPosition().y);
-    }
-    if (shape.getPosition().y > 280.00000) {
-        shape.setPosition(shape.getPosition().x, 280);
-    }
-    else if (shape.getPosition().y < 0) {
-        shape.setPosition(shape.getPosition().x,0);
-    }
+#include <vector>
+
+void Resize(sf::RenderWindow& wind, sf::View& v) {
+    float aspect = (float(wind.getSize().x) / float(wind.getSize().y));
+    v.setSize(wind.getSize().y*aspect, wind.getSize().y);
 }
+
+class Cell {
+
+};
+
+class Scene {
+
+};
+
+class Player {
+
+};
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "SFML works!");
+    sf::CircleShape shape(40.f);
     
-    sf::RenderWindow window(sf::VideoMode(300, 300), "Ultrakill 2");
-    window.setKeyRepeatEnabled(false);
-    sf::CircleShape shape(10.f);
-    sf::Text txt;
-    sf::Font fnt;
-    fnt.loadFromFile("C:\\Windows\\Fonts\\Tahoma.ttf");
-    txt.setFont(fnt); // font is a sf::Font
-
-    txt.setString("Hello world");
-
-    // set the character size
-    txt.setCharacterSize(12); // in pixels, not points!
-
-    // set the color
-    txt.setFillColor(sf::Color::Red);
-
-    // set the text style
-    txt.setStyle(sf::Text::Bold | sf::Text::Underlined);
-    sf::String coords;
-    
-    shape.setFillColor(sf::Color::Red);
-    
+    sf::Font f;
+    f.loadFromFile("C:/Windows/Fonts/Tahoma.ttf");
+   
+    shape.setFillColor(sf::Color::Green);
+    sf::RectangleShape W(sf::Vector2f(1500, 800));
+    W.setFillColor(sf::Color::White);
+    sf::View v1(sf::Vector2f(0.f,0.f), sf::Vector2f(800.f,600.f));
+    v1.setCenter(0.f, 0.f);
+    v1.setSize(1800.f, 1500.f);
     while (window.isOpen())
     {
         sf::Event event;
@@ -48,30 +39,41 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            {
-                shape.move(0, 10);
+            if (event.type == event.KeyPressed) {
+                if (event.key.code == sf::Keyboard::Right) {
+                    v1.move(10.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::Left) {
+                    v1.move(-10.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::Up) {
+                    v1.move(0.f, -10.f);
+                }
+                if (event.key.code == sf::Keyboard::Down) {
+                    v1.move(0.f, 10.f);
+                }
+                if (event.key.code == sf::Keyboard::D) {
+                    shape.move(60.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::A) {
+                    shape.move(-60.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::W) {
+                    shape.move(0.f, -60.f);
+                }
+                if (event.key.code == sf::Keyboard::S) {
+                    shape.move(0.f, 60.f);
+                }
             }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            {
-                shape.move(0, -10);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-                shape.move(10,0);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            {
-                shape.move(-10, 0);
+            if (event.type == event.Resized) {
+                Resize(window, v1);
             }
         }
-        window.clear();    
-        coords = std::to_string(shape.getPosition().x) + " " + std::to_string(shape.getPosition().y);
-        txt.setString(coords);
-        txt.setPosition(150, 150);
-        window.draw(txt);
+
+        window.clear();
+        window.draw(W);
         window.draw(shape);
-        checkBorder(shape);
+        window.setView(v1);
         window.display();
     }
 
